50f45c2cccf690509d613bd505678ccf61b6d739
JLarky <jlarky@gmail.com>
2023-04-29 20:28:05 -0600

    vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
+import vercel from '@astrojs/vercel/edge';
 
 // https://astro.build/config
 export default defineConfig({
 	integrations: [react()],
+	output: 'server',
+	adapter: vercel(),
 });
