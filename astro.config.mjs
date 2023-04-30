654c876b2e645b89eea040bc69c985835d90f7fc
JLarky <jlarky@gmail.com>
2023-04-29 21:52:13 -0600

    switch to serverless to fix build

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
-import vercel from '@astrojs/vercel/edge';
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
