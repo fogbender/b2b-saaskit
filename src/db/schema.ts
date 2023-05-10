5cd3c033a2bf27f8a2c4bef162a25f9befafb21c
JLarky <jlarky@gmail.com>
2023-05-09 20:48:07 -0600

    migration to add tables

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,4 +1,4 @@
-import { pgTable, text, timestamp } from 'drizzle-orm/pg-core';
+import { boolean, integer, pgEnum, pgTable, serial, text, timestamp } from 'drizzle-orm/pg-core';
 
 // Hello and welcome to the schema file!
 // 1. run `doppler run npx drizzle-kit generate:pg` to generate migrations
@@ -7,8 +7,8 @@ import { pgTable, text, timestamp } from 'drizzle-orm/pg-core';
 // 3. once this is done, run `doppler run yarn migrate` to apply the migration
 
 /* ```sql
-ALTER TABLE PROMPTS ENABLE ROW LEVEL SECURITY;
-CREATE POLICY "service" ON "public"."prompts" AS PERMISSIVE FOR ALL TO service_role USING (true)
+ALTER TABLE prompts ENABLE ROW LEVEL SECURITY;
+CREATE POLICY "service" ON "public"."prompts" AS PERMISSIVE FOR ALL TO service_role USING (true);
 ``` */
 
 export const prompts = pgTable('prompts', {
@@ -17,4 +17,36 @@ export const prompts = pgTable('prompts', {
 	content: text('content').notNull(),
 	privacyLevel: text('privacy_level').notNull(),
 	createdAt: timestamp('created_at').notNull(),
+});
+
+export const gptTypeEnum = pgEnum('gpt_type', ['gpt-3', 'gpt-4']);
+
+/* ```sql
+ALTER TABLE gpt_keys ENABLE ROW LEVEL SECURITY;
+CREATE POLICY "service" ON "public"."gpt_keys" AS PERMISSIVE FOR ALL TO service_role USING (true);
+``` */
+
+export const gptKeys = pgTable('gpt_keys', {
+	keyId: serial('id').primaryKey(),
+	keyPublic: text('key_public').notNull(),
+	keySecret: text('key_secret').notNull(),
+	keyType: gptTypeEnum('key_type').notNull(),
+	userId: text('user_id').notNull(),
+	orgId: text('org_id').notNull(),
+	isShared: boolean('is_shared').default(false).notNull(),
+	createdAt: timestamp('created_at').defaultNow().notNull(),
+	lastUsedAt: timestamp('last_used_at'),
+});
+
+export const itemTypeEnum = pgEnum('item_type', ['user', 'org']);
+
+/* ```sql
+ALTER TABLE default_keys ENABLE ROW LEVEL SECURITY;
+CREATE POLICY "service" ON "public"."default_keys" AS PERMISSIVE FOR ALL TO service_role USING (true);
+``` */
+
+export const defaultKeys = pgTable('default_keys', {
+	itemId: text('item_id').primaryKey(),
+	itemType: itemTypeEnum('item_type').notNull(),
+	keyId: integer('key_id').references(() => gptKeys.keyId),
 });
