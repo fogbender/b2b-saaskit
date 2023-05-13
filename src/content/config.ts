44ae552d6093a90f2b706951c24ed271363d3957
JLarky <jlarky@gmail.com>
2023-05-12 21:52:05 -0600

    clean up

--- src/content/config.ts
+++ src/content/config.ts
@@ -2,8 +2,8 @@ import { z, defineCollection } from 'astro:content';
 
 const setupCollection = defineCollection({
 	schema: z.object({
-		title: z.string().optional(),
-		needsEnv: z.array(z.string()).optional(),
+		title: z.string(),
+		needsEnv: z.array(z.string()),
 	}),
 });
 
