180b6bfb9f4ef5ce4058fa64cd4faba500bbd719
JLarky <jlarky@gmail.com>
2023-05-15 22:13:16 -0600

    redo settings

--- src/components/app/Settings.tsx
+++ src/components/app/Settings.tsx
@@ -1,11 +1,10 @@
 import { useQueryClient } from '@tanstack/react-query';
 import { getQueryKey } from '@trpc/react-query';
 import { env } from '../../config';
-import { AuthProvider } from '../propelauth';
+import { AuthProvider, useActiveOrg, useAuthInfo } from '../propelauth';
 import { TRPCProvider, trpc } from '../trpc';
 import { AuthSync } from '../AuthSync';
-import { useReducer } from 'react';
-import arrowDown from '../../assets/arrow-down.svg';
+import { useEffect, useReducer } from 'react';
 import { Layout } from './Layout';
 import { AppNav } from './Nav';
 
@@ -22,7 +21,23 @@ export function Settings() {
 }
 
 function Interal() {
-	const keyQuery = trpc.settings.getKeys.useQuery(undefined, { staleTime: 1000 });
+	const auth = useAuthInfo();
+	const activeOrg = useActiveOrg();
+	useEffect(() => {
+		if (auth.loading === false) {
+			if (!auth.user || !activeOrg) {
+				window.location.href = '/app';
+			}
+		}
+	}, [auth, activeOrg]);
+	const orgId = activeOrg?.orgId;
+	const keyQuery = trpc.settings.getKeys.useQuery(
+		{ orgId: orgId || '' },
+		{
+			enabled: !!orgId,
+			staleTime: 1000,
+		}
+	);
 	const queryClient = useQueryClient();
 	const addKeyMutation = trpc.settings.createKey.useMutation({
 		onSettled: () => {
@@ -37,11 +52,6 @@ function Interal() {
 			);
 		},
 		onSettled: () => {
-			queryClient.invalidateQueries(getQueryKey(trpc.settings.getKeys));
-		},
-	});
-	const changeVisibilityMutation = trpc.settings.changeVisibility.useMutation({
-		onSuccess: () => {
 			queryClient.invalidateQueries(getQueryKey(trpc.settings.getKeys));
 		},
 	});
@@ -64,9 +74,15 @@ function Interal() {
 									>
 										API Keys
 									</a>{' '}
-									page. You can choose if you want to share your key with other users of your
-									organization or just use it for yourself. Actual token is not going to be shared,
-									just the ability to use it through Prompts with Friends.
+									page. Be careful you are going to share access to this token to your whole{' '}
+									<a
+										className="text-blue-500 hover:text-blue-700 underline"
+										href="https://94269089.propelauthtest.com/org"
+									>
+										Team
+									</a>
+									. Actual token is not going to be shared, just the ability to use it through
+									Prompts with Friends.
 								</p>
 							</div>
 							<div className="mt-4 sm:ml-16 sm:mt-0 sm:flex-none">
@@ -75,7 +91,7 @@ function Interal() {
 									className="block rounded-md bg-indigo-600 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
 									onClick={toggleShowAddKey}
 								>
-									{showAddKey ? 'Cancel' : 'Add Key'}
+									{showAddKey ? 'Cancel' : keyQuery.data?.length ? 'Replace Key' : 'Add Key'}
 								</button>
 							</div>
 						</div>
@@ -106,6 +122,7 @@ function Interal() {
 										)
 									</span>
 								</label>
+								<input type="hidden" name="orgId" value={orgId} />
 								<input
 									className="border border-gray-300 rounded-md p-2"
 									type="text"
@@ -126,42 +143,33 @@ function Interal() {
 									<option value="gpt-4">GPT-4</option>
 								</select>
 								<button
-									className="bg-blue-500 text-white py-2 px-4 rounded-md"
+									className="bg-blue-500 text-white py-2 px-4 rounded-md disabled:opacity-50"
 									type="submit"
 									disabled={addKeyMutation.isLoading}
 								>
-									{addKeyMutation.isLoading ? 'Loading...' : 'Store'}
+									{addKeyMutation.isLoading
+										? 'Storing...'
+										: keyQuery.data?.length
+										? 'Replace'
+										: 'Store'}
 								</button>
 								{addKeyMutation.isError && (
-									<p>
+									<div>
 										Error!{' '}
 										{addKeyMutation.error?.data?.zodError && (
 											<pre>{JSON.stringify(addKeyMutation.error.data.zodError, null, 2)}</pre>
 										)}
-									</p>
+									</div>
 								)}
 							</form>
 						)}
 					</div>
-					<KeySelector type="user" keys={keyQuery.data}>
-						<h1 className="font-medium text-xl">Personal token</h1>
-						<p className="text-sm text-gray-600">
-							What key you want to be used when you run prompts.
-						</p>
-					</KeySelector>
-					<KeySelector>
-						<h1 className="font-medium text-xl">Default token for organization</h1>
-						<p className="text-sm text-gray-600">
-							What key you want to be used when somone from your organization runs prompts.
-						</p>
-					</KeySelector>
 					<div className="flex flex-col rounded-lg border border-gray-200 w-full mt-4 pb-4 gap-4 space-between divide-y divide-gray-200">
 						<div className="flex flex-row w-full px-6 pt-6 pb-2">
 							<div className="flex flex-col">
-								<h2 className="font-medium text-lg">All Keys</h2>
+								<h2 className="font-medium text-lg">Organization key</h2>
 								<p className="text-sm text-gray-600">
-									This will include all keys that are available to you, including key configured by
-									members of your organization.
+									You can see here the key that is getting used by default for your organization.
 								</p>
 							</div>
 						</div>
@@ -199,10 +207,28 @@ function Interal() {
 									</tr>
 								</thead>
 								<tbody className="divide-y divide-gray-200 bg-white">
+									{keyQuery.isLoading && (
+										<tr>
+											<td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-6 lg:pl-8">
+												Loading...
+											</td>
+										</tr>
+									)}
 									{!keyQuery.isLoading && keyQuery.data?.length === 0 && (
 										<tr>
 											<td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-6 lg:pl-8">
-												No keys are configured, add one to start using prompts.
+												No keys are configured,{' '}
+												<a
+													href="#"
+													className="text-blue-500 hover:text-blue-700 underline"
+													onClick={(e) => {
+														e.preventDefault();
+														toggleShowAddKey();
+													}}
+												>
+													add one
+												</a>{' '}
+												to start using prompts.
 											</td>
 										</tr>
 									)}
@@ -231,23 +257,6 @@ function Interal() {
 											<td className="flex relative whitespace-nowrap py-4 justify-end text-sm font-medium gap-2 pr-4">
 												<button
 													type="button"
-													className={`text-indigo-600 hover:text-indigo-900 p-1 focus:outline-indigo-500 ${
-														changeVisibilityMutation.isLoading &&
-														changeVisibilityMutation.variables?.keyId === key.keyId
-															? 'opacity-50'
-															: ''
-													}`}
-													onClick={() => {
-														changeVisibilityMutation.mutate({
-															keyId: key.keyId,
-															isShared: !key.isShared,
-														});
-													}}
-												>
-													{key.isShared ? 'Make private' : 'Share'}
-												</button>
-												<button
-													type="button"
 													className="text-indigo-600 hover:text-indigo-900 p-1 focus:outline-indigo-500"
 													onClick={() => {
 														const confirm = window.confirm(
@@ -272,53 +281,3 @@ function Interal() {
 		</Layout>
 	);
 }
-
-const KeySelector = ({
-	type,
-	keys,
-	children,
-}: {
-	type?: 'user';
-	keys?: {
-		keyId: number;
-		keyPublic: string;
-	}[];
-	children: React.ReactNode;
-}) => {
-	const myDefaultsQuery = trpc.settings.getDefaults.useQuery();
-	const saveDefaultMutation = trpc.settings.saveDefault.useMutation({
-		onSettled: () => {
-			myDefaultsQuery.refetch();
-		},
-	});
-	const selectedValue = myDefaultsQuery?.data?.find((x) => x.itemType === type);
-	return (
-		<div className="flex flex-col w-full gap-2 p-4 bg-gray-100 rounded-lg mt-6">
-			<div className="flex flex-row items-center justify-between">
-				<div className="flex flex-col text-start w-full gap-2">{children}</div>
-				<select
-					className={`mt-2 block rounded-md border-0 py-1.5 pl-3 pr-10 text-gray-900 ring-1 ring-inset ring-gray-300 focus:ring-indigo-600 accent-indigo-600 focus:outline-indigo-500 sm:text-sm sm:leading-6 appearance-none bg-no-repeat ${
-						saveDefaultMutation.isLoading || myDefaultsQuery.isLoading ? 'opacity-50' : ''
-					}`}
-					style={{
-						backgroundImage: 'url(' + arrowDown + ')',
-						backgroundPosition: 'right 8px center',
-					}}
-					value={myDefaultsQuery.data?.[0]?.keyId ?? 0}
-					onChange={(e) => {
-						const keyId = Number(e.target.value);
-						if (keyId === 0) return;
-						saveDefaultMutation.mutate({ keyId, itemType: 'user' });
-					}}
-				>
-					{!selectedValue && <option value={0}>Select a key</option>}
-					{keys?.map((key) => (
-						<option key={key.keyId} value={key.keyId}>
-							{key.keyPublic}
-						</option>
-					))}
-				</select>
-			</div>
-		</div>
-	);
-};
--- src/lib/trpc/routers/settings.ts
+++ src/lib/trpc/routers/settings.ts
@@ -4,50 +4,66 @@ import { eq } from 'drizzle-orm';
 import { createTRPCRouter, authProcedure } from '../trpc';
 import { TRPCError } from '@trpc/server';
 import { db } from '../../../db/db';
-import { gptKeys, defaultKeys } from '../../../db/schema';
+import { gptKeys } from '../../../db/schema';
 import type { User } from '@propelauth/node';
 
 export const settingsRouter = createTRPCRouter({
-	getKeys: authProcedure.query(async ({}) => {
-		return await db
-			.select({
-				keyId: gptKeys.keyId,
-				keyType: gptKeys.keyType,
-				createdAt: gptKeys.createdAt,
-				lastUsedAt: gptKeys.lastUsedAt,
-				keyPublic: gptKeys.keyPublic,
-				isShared: gptKeys.isShared,
+	getKeys: authProcedure
+		.input(
+			z.object({
+				orgId: z.string(),
 			})
-			.from(gptKeys)
-			.orderBy(gptKeys.createdAt);
-	}),
+		)
+		.query(async ({ ctx, input }) => {
+			requireOrg(ctx, input.orgId);
+			return await db
+				.select({
+					keyId: gptKeys.keyId,
+					keyType: gptKeys.keyType,
+					createdAt: gptKeys.createdAt,
+					lastUsedAt: gptKeys.lastUsedAt,
+					keyPublic: gptKeys.keyPublic,
+					isShared: gptKeys.isShared,
+				})
+				.from(gptKeys)
+				.where(eq(gptKeys.orgId, input.orgId))
+				.orderBy(gptKeys.createdAt);
+		}),
 	createKey: authProcedure
 		.input(
 			z.object({
+				orgId: z.string(),
 				keySecret: z.string(),
 				keyType: z.enum(['gpt-3', 'gpt-4']),
 			})
 		)
 		.mutation(async ({ ctx, input }) => {
-			const x = await db
-				.insert(gptKeys)
-				.values({
-					userId: ctx.user.userId,
-					keySecret: input.keySecret,
-					keyPublic: input.keySecret.slice(0, 3) + '...' + input.keySecret.slice(-4),
-					keyType: input.keyType,
-					orgId: 'public',
-					isShared: false,
-				})
-				.returning({ id: gptKeys.keyId });
-			const key = x[0];
-			if (!key) {
+			requireOrg(ctx, input.orgId);
+			let keyId: number | undefined;
+			await db.transaction(async (trx) => {
+				await trx.delete(gptKeys).where(eq(gptKeys.orgId, input.orgId));
+				const y = await trx
+					.insert(gptKeys)
+					.values({
+						userId: ctx.user.userId,
+						keySecret: input.keySecret,
+						keyPublic: input.keySecret.slice(0, 3) + '...' + input.keySecret.slice(-4),
+						keyType: input.keyType,
+						orgId: input.orgId,
+						isShared: true,
+					})
+					.returning({ id: gptKeys.keyId });
+				if (y[0]) {
+					keyId = y[0].id;
+				}
+			});
+			if (!keyId) {
 				throw new TRPCError({
 					code: 'INTERNAL_SERVER_ERROR',
-					message: 'Failed to create key',
+					message: 'Failed to store the key',
 				});
 			}
-			return key.id;
+			return keyId;
 		}),
 	deleteKey: authProcedure
 		.input(
@@ -78,84 +94,26 @@ export const settingsRouter = createTRPCRouter({
 				});
 			}
 			await db.transaction(async (trx) => {
-				await trx.delete(defaultKeys).where(eq(defaultKeys.keyId, input.keyId));
 				await trx.delete(gptKeys).where(eq(gptKeys.keyId, input.keyId));
 			});
-			return input;
-		}),
-	changeVisibility: authProcedure
-		.input(
-			z.object({
-				keyId: z.number(),
-				isShared: z.boolean(),
-			})
-		)
-		.mutation(async ({ ctx, input }) => {
-			const x = await db
-				.select({
-					userId: gptKeys.userId,
-					orgId: gptKeys.orgId,
-				})
-				.from(gptKeys)
-				.where(eq(gptKeys.keyId, input.keyId));
-			const item = x[0];
-			if (!item) {
-				throw new TRPCError({
-					code: 'NOT_FOUND',
-					message: 'Prompt not found',
-				});
-			}
-
-			if (!canOnlyChangeOwnKey(ctx, item)) {
-				throw new TRPCError({
-					code: 'FORBIDDEN',
-					message: 'You can only edit your own prompts.',
-				});
-			}
-			await db
-				.update(gptKeys)
-				.set({ isShared: input.isShared })
-				.where(eq(gptKeys.keyId, input.keyId));
-			return input;
-		}),
-	getDefaults: authProcedure.query(async ({ ctx }) => {
-		return await db
-			.select({
-				keyId: defaultKeys.keyId,
-				itemType: defaultKeys.itemType,
-				itemId: defaultKeys.itemId,
-			})
-			.from(defaultKeys)
-			.where(eq(defaultKeys.itemId, ctx.user.userId))
-			.orderBy(defaultKeys.keyId);
-	}),
-	saveDefault: authProcedure
-		.input(
-			z.object({
-				keyId: z.number(),
-				itemType: z.enum(['user', 'org']),
-			})
-		)
-		.mutation(async ({ ctx, input }) => {
-			console.error('FIXME: check access');
-			// if (!canOnlyChangeOwnKey(ctx, item)) {
-			// 	throw new TRPCError({
-			// 		code: 'FORBIDDEN',
-			// 		message: 'You can only edit your own prompts.',
-			// 	});
-			// }
-			const itemId = input.itemType === 'user' ? ctx.user.userId : 'ctx.user.orgId';
-			await db
-				.insert(defaultKeys)
-				.values({ itemId, itemType: input.itemType, keyId: input.keyId })
-				.onConflictDoUpdate({
-					target: [defaultKeys.itemType, defaultKeys.itemId],
-					set: { keyId: input.keyId },
-				});
 			return input;
 		}),
 });
 
 function canOnlyChangeOwnKey(ctx: { user: User }, item: { userId: string; orgId: string }) {
 	return item?.userId === ctx.user.userId;
+}
+
+function requireOrg(ctx: { user: User }, requiredOrgId: string) {
+	for (const orgId in ctx.user.orgIdToOrgMemberInfo) {
+		const orgMemberInfo = ctx.user.orgIdToOrgMemberInfo[orgId];
+		console.log(orgMemberInfo);
+		if (orgMemberInfo?.orgId === requiredOrgId) {
+			return orgMemberInfo;
+		}
+	}
+	throw new TRPCError({
+		code: 'FORBIDDEN',
+		message: 'You can only view key of your own org.',
+	});
 }
