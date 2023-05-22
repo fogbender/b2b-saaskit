82479723292fa9587d175e9276a9b1c0ae83bcf7
JLarky <jlarky@gmail.com>
2023-05-21 19:39:51 -0600

    astro.config eslint

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
-import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import vercel from '@astrojs/vercel/serverless';
+import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
 	integrations: [react()],
 	output: 'server',
 	adapter: vercel(),
+	// eslint-disable-next-line no-undef
 	site: process.env.SITE_URL,
 });
