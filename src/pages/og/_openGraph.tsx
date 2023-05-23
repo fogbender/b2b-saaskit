1c1f9a37a0dfb3757aa17c737585cb9d0ef40aa6
karannakra <karannakra1@gmail.com>
2023-05-24 02:01:21 +0530

    give alt name to img tag

--- src/pages/og/_openGraph.tsx
+++ src/pages/og/_openGraph.tsx
@@ -18,6 +18,7 @@ export default function OpenGraph({ title, description, originUrl }: OpenGraphPr
 			}}
 		>
 			<img
+				alt="background"
 				src={originUrl + '/og/background.png'}
 				style={{
 					position: 'absolute',
