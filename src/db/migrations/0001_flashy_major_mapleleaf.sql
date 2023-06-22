aeec64db2f949d0b1604c548a7be616b09a5b4b4
JLarky <jlarky@gmail.com>
2023-06-22 11:00:51 -0600

    drop migrations from PR

--- src/db/migrations/0001_flashy_major_mapleleaf.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT EXISTS "org_stripe_customer_mappings" (
-	"org_id" text NOT NULL,
-	"stripe_customer_id" text NOT NULL
-);
---> statement-breakpoint
-ALTER TABLE "org_stripe_customer_mappings" ADD CONSTRAINT "org_stripe_customer_mappings_org_id_stripe_customer_id" PRIMARY KEY("org_id","stripe_customer_id");
-
-ALTER TABLE org_stripe_customer_mappings ENABLE ROW LEVEL SECURITY;
-CREATE POLICY "service" ON "public"."org_stripe_customer_mappings" AS PERMISSIVE FOR ALL TO service_role USING (true);
--- src/db/migrations/0002_remarkable_butterfly.sql
+++
@@ -1,1 +0,0 @@
-DROP TABLE org_stripe_customer_mappings;
