7f4318af62f20f57458d948b3d1a754dd38fee55
Karan Nakra <karannakra1@gmail.com>
2023-06-25 20:38:35 +0530

    clean up rules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -70,14 +70,15 @@ module.exports = {
 		'simple-import-sort/exports': 'error',
 		'deprecation/deprecation': 'error',
 
-		'@typescript-eslint/await-thenable': 'off',
 		'@typescript-eslint/no-unsafe-call': 'off',
 		'@typescript-eslint/no-explicit-any': 'off',
-		'@typescript-eslint/no-unused-vars': 'off',
+		'@typescript-eslint/no-unused-vars': [
+			'warn',
+			{ argsIgnorePattern: '^_', varsIgnorePattern: '^_' },
+		],
 		'@typescript-eslint/no-non-null-assertion': 'off',
 		'@typescript-eslint/no-empty-function': 'off',
 		'typescript-sort-keys/interface': 'off',
-		'no-empty-pattern': 'error',
 
 		// Stylistic concerns that don't interfere with Prettier
 		'no-mixed-spaces-and-tabs': 'off',
