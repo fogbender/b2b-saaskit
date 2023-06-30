686c7c51cb097758db693a1ba396f8abd1d5c666
JLarky <jlarky@gmail.com>
2023-06-29 20:44:49 -0600

    refactor code into usePromptErrorPage

--- src/components/app/Prompt.tsx
+++ src/components/app/Prompt.tsx
@@ -6,6 +6,7 @@ import { websiteTitle } from '../../constants';
 import type { PromptPrivacyLevel } from '../../lib/trpc/routers/prompts';
 import { RouterOutput, trpc } from '../trpc';
 import { Layout } from './Layout';
+import { usePromptErrorPage } from './usePromptErrorPage';
 import { Message, resolveTemplates, useRedirectToLoginPage } from './utils';
 
 export function Prompt() {
@@ -36,49 +37,14 @@ export function Prompt() {
 			trpcUtils?.prompts.getPrompt.invalidate({ promptId });
 		},
 	});
-	const errorCode = promptQuery.error?.data?.code;
-	// to make error message sticky when react query tries to refetch
-	const [stickyErrorCode, setStickyErrorCode] = useState(errorCode);
-	useEffect(() => {
-		if (errorCode) {
-			setStickyErrorCode(errorCode);
-		}
-	}, [errorCode]);
-	useEffect(() => {
-		if (promptQuery.status === 'success') {
-			setStickyErrorCode(undefined);
-		}
-	}, [promptQuery.status]);
+
+	const errorPage = usePromptErrorPage(promptQuery.status, promptQuery.error?.data?.code);
+
+	if (errorPage) {
+		return errorPage;
+	}
 
 	const data = promptQuery.data;
-
-	if (stickyErrorCode) {
-		if (stickyErrorCode === 'FORBIDDEN') {
-			return (
-				<Layout title="You don't have access to this prompt 😭">
-					<div className="mt-5">
-						You can only view prompts that are public, shared within your team, or created by you
-					</div>
-				</Layout>
-			);
-		} else if (stickyErrorCode === 'UNAUTHORIZED') {
-			return (
-				<Layout title="You need to be logged in to view this prompt">
-					Unauthorized users can only view public prompts. Please log in to view this prompt.
-				</Layout>
-			);
-		} else if (stickyErrorCode === 'NOT_FOUND') {
-			return (
-				<Layout title="Prompt not found">We couldn't find the prompt you were looking for.</Layout>
-			);
-		} else {
-			return (
-				<Layout title="Error">
-					An error occurred while loading the prompt. Please try again later.
-				</Layout>
-			);
-		}
-	}
 
 	return (
 		<Layout>
