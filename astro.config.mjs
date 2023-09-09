74712cf69e91858e3fecc34fa952abb5806ddaad
JLarky <jlarky@gmail.com>
2023-09-09 17:48:03 -0600

    add vite-plugin-checker
    - it only checks inside typescript files (not astro files)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import react from '@astrojs/react';
 import vercel from '@astrojs/vercel/serverless';
 import { defineConfig } from 'astro/config';
+import checker from 'vite-plugin-checker';
 
 // https://astro.build/config
 export default defineConfig({
@@ -12,6 +13,13 @@ export default defineConfig({
 	// eslint-disable-next-line no-undef
 	site: process.env.SITE_URL,
 	vite: {
+		plugins: [
+			checker({
+				typescript: true,
+				overlay: { initialIsOpen: false, badgeStyle: 'left: 55px; bottom: 8px;' },
+				enableBuild: false, // we already check that in `yarn ci:check`
+			}),
+		],
 		optimizeDeps: {
 			exclude: ['@resvg/resvg-js'],
 		},
