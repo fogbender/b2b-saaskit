a2b271d96a7b6504e69e22164f4f6e74e0a9aa09
JLarky <jlarky@gmail.com>
2023-11-01 00:48:38 -0600

    WIP

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,29 @@ import { unstable_vitePlugin as remix } from '@remix-run/dev';
 import { defineConfig } from 'astro/config';
 import checker from 'vite-plugin-checker';
 
+const x = remix({});
+
+x.forEach((y) => {
+	console.log(y.name);
+});
+
+const remixVite = x.filter(
+	(y) => !['remix-hmr-updates', 'remix-hmr-runtime', 'remix-react-refresh-babel'].includes(y.name)
+);
+
+// console.log(x);
+
+// console.log(x[0].configureServer);
+// const orig = x[0].configureServer;
+// x[0].configureServer = (server) => {
+// 	console.log('configureServer', server);
+// 	// return orig(server);
+// 	server.middlewares.use((req, res, next) => {
+// 		console.log('middleware', req, res, next);
+// 		next();
+// 	});
+// };
+
 // https://astro.build/config
 export default defineConfig({
 	integrations: [react()],
@@ -15,12 +38,12 @@ export default defineConfig({
 	site: process.env.SITE_URL,
 	vite: {
 		plugins: [
+			remixVite,
 			checker({
 				typescript: true,
 				overlay: { initialIsOpen: false, badgeStyle: 'left: 55px; bottom: 8px;' },
 				enableBuild: false, // we already check that in `yarn ci:check`
 			}),
-			remix(),
 		],
 		optimizeDeps: {
 			exclude: ['@resvg/resvg-js'],
