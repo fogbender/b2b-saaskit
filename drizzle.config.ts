2c2eb09935d21589e9765fecf71dd1a5ec078688
JLarky <jlarky@gmail.com>
2024-08-11 18:03:33 -0600

    update drizzle

---
+++ drizzle.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from 'drizzle-kit';
+
+export const migrationsFolder = './src/db/migrations';
+
+export default defineConfig({
+	dialect: 'postgresql', // supports "sqlite" | "mysql"
+	schema: './src/db',
+	out: migrationsFolder,
+	breakpoints: false,
+});
